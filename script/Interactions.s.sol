57f2b2b6d469dad20ae2bcd511f842c1a5739d33
chauhan-varun <varunchauhan097@gmail.com>
2025-07-21 09:08:21 +0530

    Refactor Interactions contract to store most recent deployer address and simplify minting function. Update run method to directly use the stored address for NFT minting.

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -7,18 +7,20 @@ import {Script} from "forge-std/Script.sol";
 
 contract Interactions is Script {
     PrimeNft primeNft;
+    address public mostRecentDeployer;
+
     string public constant BEAR =
         "https://ipfs.io/ipfs/bafybeibe7vdybu2gnjshiriooa2vv27jr6ur76opcqrjeyykw2w7ij2stm/bear.json";
 
     function run() external {
-        address mostRecentDeployer = DevOpsTools.get_most_recent_deployment(
+        mostRecentDeployer = DevOpsTools.get_most_recent_deployment(
             "PrimeNft",
             block.chainid
         );
-        mintNftOnContract(mostRecentDeployer);
+        mintNftOnContract();
     }
 
-    function mintNftOnContract(address mostRecentDeployer) public {
+    function mintNftOnContract() public {
         vm.startBroadcast();
         PrimeNft(mostRecentDeployer).mintNft(BEAR);
         vm.stopBroadcast();
